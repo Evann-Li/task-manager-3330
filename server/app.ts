b81efa22f110b317d7cacd125a47677c47ebf65f
Evann Li <evannli450@gmail.com>
2024-11-28 12:14:30 -0800

    set up kinde auth

--- server/app.ts
+++ server/app.ts
@@ -2,13 +2,14 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { tasksRoute  } from './routes/tasks'
 import { serveStatic } from 'hono/bun'
+import { authRoute } from './routes/auth'
 
 const app = new Hono()
 
 app.use('*', logger())
 
 const apiRoutes = app.basePath("/api")
-.route("/tasks", tasksRoute )
+.route("/tasks", tasksRoute ).route("/", authRoute)
 
 app.get('*', serveStatic({ root: './frontend/dist' }))
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
